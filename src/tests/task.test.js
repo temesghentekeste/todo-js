734b84d071aa422be8e9b6a50d5c5d293906e87f
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-18 22:00:37 +0300

    Add test for task validation

--- src/tests/task.test.js
+++ src/tests/task.test.js
@@ -7,7 +7,7 @@ const createTask = () => {
     'Project 1 Description',
     new Date(),
     'Low',
-    new Date()
+    new Date(),
   );
 };
 
@@ -15,4 +15,21 @@ beforeAll(() => createTask());
 
 test('it should create a new project', () => {
   expect(task.name).toBe('Task 1');
+});
+
+describe('Test task validation', () => {
+  test('it should return false for invalid task', () => {
+    task.name = '';
+
+    const isValid = task.validate();
+
+    expect(isValid).not.toBe(true);
+  });
+
+  test('it should return true for valid task', () => {
+    task.name = 'Task 1';
+    const isValid = task.validate();
+
+    expect(isValid).toBe(true);
+  });
 });
