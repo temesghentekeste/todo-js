3b47ea310bd4fb578543da461c08cbee3eedd50e
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-18 21:26:53 +0300

    Test setting current project

--- src/tests/project.test.js
+++ src/tests/project.test.js
@@ -5,3 +5,12 @@ test('it should create a new project', () => {
   const project = new Project('project 1', 'project 1 desc');
   expect(project.name).toBe('project 1');
 });
+
+test('it should set the current project', () => {
+  const project = new Project('project 1', 'project 1 desc');
+  
+  project.save()
+  
+  const currentProject = project.getCurrentProject();
+  expect(project.name).toBe(currentProject.name);
+});
