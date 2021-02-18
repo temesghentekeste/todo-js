6e3cb86da163791ec4734b8460653c62e1d34ffd
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-18 21:12:20 +0300

    Add test for creating a new project

--- src/tests/project.test.js
+++ src/tests/project.test.js
@@ -1,2 +1,7 @@
+const { default: Project } = require('../model/project');
 
 
+test('it should create a new project', () => {
+  const project = new Project('project 1', 'project 1 desc');
+  expect(project.name).toBe('project 1');
+});
