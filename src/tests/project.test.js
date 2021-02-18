abc8fec8d6dec6f4c4876a08655b4a7dc5637d31
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-18 21:45:17 +0300

    Refactor project tests

--- src/tests/project.test.js
+++ src/tests/project.test.js
@@ -1,32 +1,41 @@
 const { default: Project } = require('../model/project');
 
 
+let project;
+const createProject = () => {
+  project = new Project('Project 1', 'Project 1 Description');
+}
+
+
+beforeAll(() => createProject());
+
 test('it should create a new project', () => {
-  const project = new Project('project 1', 'project 1 desc');
-  expect(project.name).toBe('project 1');
+  expect(project.name).toBe('Project 1');
 });
 
 test('it should set the current project', () => {
-  const project = new Project('project 1', 'project 1 desc');
-  
-  project.save()
-  
+
+  project.save();
+
   const currentProject = project.getCurrentProject();
   expect(project.name).toBe(currentProject.name);
 });
 
-test('it should return false for invalid project', () => {
-  const project = new Project('project 1', '');
+describe('Test project validation', () => {
+  test('it should return false for invalid project', () => {
+    project.description = ''
+  
+    const isValid = project.validate();
+  
+    expect(isValid).not.toBe(true);
+  });
+  
+  test('it should return true for valid project', () => {
+  
+    project.description = 'Some descriptoin'
+    const isValid = project.validate();
+  
+    expect(isValid).toBe(true);
+  });
 
-  const isValid = project.validate();
-
-  expect(isValid).not.toBe(true);
-});
-
-test('it should return true for valid project', () => {
-  const project = new Project('project 1', 'project 1 description');
-
-  const isValid = project.validate();
-
-  expect(isValid).toBe(true);
-});
+})
