3516cce8806d72d66e4e8359f039b8070097dcab
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2021-02-19 11:42:19 +0300

    Add test for update project

--- src/tests/db.test.js
+++ src/tests/db.test.js
@@ -11,12 +11,32 @@ const initializeDb = () => {
 
 beforeAll(() => initializeDb());
 
-it('should return a project by its id', () => {
-  const currentProject = db.getProject(project.id);
-  expect(project.id).toBe(currentProject.id);
+describe('create', () => {
+  it('should save a new project', () => {
+    const newProject = new Project('Project II', 'Project II Description');
+    db.saveProject(newProject);
+    const current = db.getProject(newProject.id);
+    expect(newProject.id).toBe(current.id);
+  });
 });
 
-it('should return all projects', () => {
-  const projects = db.getProjects();
-  expect(projects.length).toBe(2);
+describe('Read', () => {
+  it('should return a project by its id', () => {
+    const currentProject = db.getProject(project.id);
+    expect(project.id).toBe(currentProject.id);
+  });
+
+  it('should return all projects', () => {
+    const projects = db.getProjects();
+    expect(projects.length).not.toBe(0);
+  });
+});
+
+describe('Update', () => {
+  it('should update the name of a project', () => {
+    project.name = 'React Calculator';
+    db.updateProject(project);
+    const currentProject = db.getProject(project.id);
+    expect(project.name).toBe(currentProject.name);
+  });
 });
